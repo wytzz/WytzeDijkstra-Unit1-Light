c0800e641469d810e17375aa8718e2875a51116a
wytzz <wytzeaz@hotmail.com>
2018-11-01 13:04:20 +0100

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Wytze Dijkstra on 01/11/2018.
+//  Copyright © 2018 Wytze Dijkstra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
