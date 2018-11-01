98a86784a5ab35ba1b04f55f73ada1a4381bce86
wytzz <wytzeaz@hotmail.com>
2018-11-01 14:36:58 +0100

    All my files

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI()
     }
-
-
+    @IBAction func buttonPressed(_ sender: AnyObject) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
 }
-
